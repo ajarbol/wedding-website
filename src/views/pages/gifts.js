8fe8027f52f7f3d8d0f4c128c4348ab50c520131
Andreas Jarbol <andreas@fitbay.com>
2018-03-09 13:26:56 +0100

    Fixed social links and added favicon

--- src/views/pages/gifts.js
+++ src/views/pages/gifts.js
@@ -5,10 +5,11 @@ import { Link } from 'preact-router';
 export default props => {
   return (
     <article className="page page__gifts">
-      <div className="page__background" />
       <Link href="/" className="hot_corner" />
-      <div className="content">
-        <img alt="Gavelisten" src="/img/headers/gifts@3x.png" srcSet="/img/headers/gifts.png, /img/headers/gifts@2x.png 2x, /img/headers/gifts@3x.png 3x" />
+      <div className="page_wrapper">
+        <div className="content">
+          <img alt="Gavelisten" src="/img/headers/gifts@3x.png" srcSet="/img/headers/gifts.png, /img/headers/gifts@2x.png 2x, /img/headers/gifts@3x.png 3x" />
+        </div>
       </div>
     </article>
   );
